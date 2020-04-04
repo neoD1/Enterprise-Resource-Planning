17a0959acebd2dc4306f649791785f2317c0f7b8
John Doe <abstract.swift.ninja@gmail.com>
2020-04-04 13:01:53 +0530

    Base Project Initial Commit

---
+++ ERP Codebase/ERP Codebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ERP Codebase
+//
+//  Created by Frank Castle on 04/04/20.
+//  Copyright © 2020 Code Bew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
